84784ff36f051852f2ca363ddbda1927efe1c038
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-01-29 15:04:38 -0500

    chore: init ms-persona spring initializr

---
+++ src/test/java/com/reto/ms_persona/MsPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.ms_persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
